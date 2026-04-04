ff0b9158088ea13131dde766f0242985df56f693
Lu Quoc Khai Ho <holuquockhai@gmail.com>
2026-04-04 16:08:44 +1100

    integrate login feature to dev branch

--- fast-api/app/main.py
+++ fast-api/app/main.py
@@ -8,16 +8,20 @@ from app.services.seed_service import seed_admin
 
 app = FastAPI(title="FastAPI RBAC")
 
-#add Middleware for Cors
+# List the origins (URL of your React app) allowed to make requests
+origins = [
+    "http://localhost:3000",  # Default React port
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",  # Default Vite port
+    "http://127.0.0.1:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=origins,       # Use ["*"] to allow all (not recommended for production)
+    allow_credentials=True,      # Allow cookies and authentication headers
+    allow_methods=["*"],         # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],         # Allow all headers
 )
 
 Base.metadata.create_all(bind=engine)
--- fast-api/app/services/auth_service.py
+++ fast-api/app/services/auth_service.py
@@ -1,4 +1,3 @@
-from pydantic.v1 import EmailStr
 from sqlalchemy import select
 from sqlalchemy.orm import Session, selectinload
 
@@ -18,7 +17,7 @@ def get_user_by_email(db: Session, email: str) -> User | None:
 
 def register_user(
     db: Session,
-    email: EmailStr,
+    email: str,
     password: str,
     full_name: str | None = None,
 ) -> User:
@@ -47,7 +46,7 @@ def register_user(
     return user
 
 
-def authenticate_user(db: Session, email: EmailStr, password: str):
+def authenticate_user(db: Session, email: str, password: str):
     user = get_user_by_email(db, email)
 
     if not user:
@@ -62,7 +61,7 @@ def authenticate_user(db: Session, email: EmailStr, password: str):
     return user, None
 
 
-def login_user(db: Session, email: EmailStr, password: str):
+def login_user(db: Session, email: str, password: str):
     user, error = authenticate_user(db, email, password)
 
     if error:
