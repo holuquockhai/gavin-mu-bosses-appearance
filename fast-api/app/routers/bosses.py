7d86133d5b91e0f07c0ce7fb75909f8d9b52fedc
Lu Quoc Khai Ho <holuquockhai@gmail.com>
2026-04-09 03:05:40 +1000

    update create boss function api

--- fast-api/app/routers/bosses.py
+++ fast-api/app/routers/bosses.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 
 from app.db.database import get_db
 from app.schemas.boss import BossCreate, BossResponse
-from app.services.boss_service import create_boss, get_bosses, get_boss, delete_boss
+from app.services.boss_service import create_boss, get_bosses, get_bosse_by_name, get_boss, delete_boss
 from app.dependencies.auth import require_permissions
 
 router = APIRouter(prefix="/bosses", tags=["bosses"])
@@ -13,6 +13,12 @@ router = APIRouter(prefix="/bosses", tags=["bosses"])
 @router.post("/", response_model=BossResponse,
              dependencies=[Depends(require_permissions(["boss:create"]))])
 def create(data: BossCreate, db: Annotated[Session, Depends(get_db)]):
+    boss = get_bosse_by_name(db, data.name)
+
+    #Check if boss exist or not, then create new boss
+    if boss:
+        raise HTTPException(status_code=400, detail="Boss already exists")
+
     return create_boss(db, data.name)
 
 
@@ -20,7 +26,6 @@ def create(data: BossCreate, db: Annotated[Session, Depends(get_db)]):
             dependencies=[Depends(require_permissions(["boss:read"]))])
 def list_all(db: Annotated[Session, Depends(get_db)]):
     return get_bosses(db)
-
 
 @router.get("/{boss_id}", response_model=BossResponse,
             dependencies=[Depends(require_permissions(["boss:read"]))])
--- fast-api/app/services/boss_service.py
+++ fast-api/app/services/boss_service.py
@@ -1,7 +1,6 @@
 from sqlalchemy.orm import Session
 from app.models.boss import Boss
 
-
 def create_boss(db: Session, name: str) -> Boss:
     boss = Boss(name=name)
     db.add(boss)
@@ -17,6 +16,8 @@ def get_bosses(db: Session):
 def get_boss(db: Session, boss_id: int):
     return db.query(Boss).filter(Boss.id == boss_id).first()
 
+def get_bosse_by_name(db: Session, name: str):
+    return db.query(Boss).filter(Boss.name == name).first()
 
 def delete_boss(db: Session, boss_id: int):
     boss = get_boss(db, boss_id)
